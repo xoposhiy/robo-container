f15f49837b830f5c57b6963d9ad78ef3f40951e7
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2009-11-10 19:33:16 +0000

    Создана инфраструктура для вставки кода тестов в документацию. Начало работы над QuickStart.wiki

---
+++ trunk/RoboContainer.Tests/SamplesForWiki/QuickStart/QuickStart_Tests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RoboContainer.Tests.SamplesForWiki.QuickStart
+{
+	[TestFixture]
+	public class QuickStart_Tests
+	{
+		//[FirstSample
+		public interface IDistanceSensor
+		{
+			// ...
+		}
+
+		public class OpticalSensor : IDistanceSensor
+		{
+			// ...
+		}
+
+		public interface IRobot
+		{
+			IDistanceSensor DistanceSensor { get; set; }
+			// ...
+		}
+
+		public class Robot : IRobot
+		{
+			public IDistanceSensor DistanceSensor { get; set; }
+			
+			public Robot(IDistanceSensor distanceSensor)
+			{
+				DistanceSensor = distanceSensor;
+			}
+
+			// ...
+		}
+		//]
+
+		[Test]
+		public void AssembleRobot()
+		{
+			//[FirstSample
+			var container = new Container();
+			IRobot robot = container.Get<IRobot>();
+			Assert.IsInstanceOf<Robot>(robot);
+			Assert.IsInstanceOf<OpticalSensor>(robot.DistanceSensor);
+			//]
+		}
+
+		//[Weapons
+		public interface IMainWeapon
+		{
+			// ...
+		}
+
+		public class RocketWeapon : IMainWeapon
+		{
+			// ...
+		}
+
+		public class LaserWeapon : IMainWeapon
+		{
+			// ...
+		}
+		//]
+
+		[Test]
+		public void WeaponConfiguration()
+		{
+			//[Weapons
+			var container = new Container(c => c.ForPlugin<IMainWeapon>().PluggableIs<RocketWeapon>());
+			IMainWeapon weapon = container.Get<IMainWeapon>();
+			Assert.IsInstanceOf<RocketWeapon>(weapon);
+			//]
+		}
+
+		[Test]
+		public void SingletoneWeapon()
+		{
+			//[SingletoneWeapon
+			var container = new Container(c => c.ForPlugin<IMainWeapon>().PluggableIs<RocketWeapon>());
+			IMainWeapon weapon1 = container.Get<IMainWeapon>();
+			IMainWeapon weapon2 = container.Get<IMainWeapon>();
+			Assert.AreSame(weapon1, weapon2);
+			//]
+		}
+
+		[Test]
+		public void TransientWeapon()
+		{
+			//[TransientWeapon
+			var container = new Container(c => c.ForPlugin<IMainWeapon>().PluggableIs<RocketWeapon>().SetScope(InstanceLifetime.PerRequest));
+			IMainWeapon weapon1 = container.Get<IMainWeapon>();
+			IMainWeapon weapon2 = container.Get<IMainWeapon>();
+			Assert.AreNotSame(weapon1, weapon2); // NOT Same
+			//]
+		}
+
+	}
+
+}
