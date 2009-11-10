088ece069a58bbf3e8e8e93cde3ff786fcfa14e4
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2009-11-10 19:33:16 +0000

    Создана инфраструктура для вставки кода тестов в документацию. Начало работы над QuickStart.wiki

---
+++ RoboContainer.Tests/SamplesForWiki/IncludeSamplesIntoWikiPages_Test.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace RoboContainer.Tests.SamplesForWiki
+{
+	[TestFixture]
+	public class IncludeSamplesIntoWikiPages_Test
+	{
+		private static DirectoryInfo LocateWikiDirectory()
+		{
+			var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+			while (dir != null && !dir.GetDirectories("wiki").Any())
+				dir = dir.Parent;
+			if (dir == null) return null;
+			return dir.GetDirectories("wiki").First();
+		}
+
+		[Test]
+		public void Test_PageProcessor()
+		{
+			var processor = 
+				new PageProcessor("asdasd <wiki:comment>[key1</wiki:comment> hello <wiki:comment /> sdfsdf<wiki:comment>[key2</wiki:comment><wiki:comment />");
+			string result = processor.Replace(new Dictionary<string, string> {{"key1", "value1"}, {"key2", "value2"}});
+			Console.WriteLine(result);
+			Assert.AreEqual(
+				"asdasd <wiki:comment>[key1</wiki:comment>{{{value1}}}<wiki:comment /> sdfsdf<wiki:comment>[key2</wiki:comment>{{{value2}}}<wiki:comment />", 
+				result);
+		}
+
+		[Test]
+		public void Include()
+		{
+			DirectoryInfo wikiDirectory = LocateWikiDirectory();
+			if (wikiDirectory == null)
+			{
+				Assert.Ignore("Can't find wiki directory");
+				return;
+			}
+			IDictionary<string, string> samples = LoadSamples();
+			foreach (var sample in samples)
+			{
+				Console.WriteLine(sample.Key);
+				Console.WriteLine(sample.Value);
+				Console.WriteLine();
+			}
+			foreach(var pageFile in wikiDirectory.GetFiles("*.wiki"))
+				ProcessPage(pageFile, samples);
+		}
+
+		private readonly Regex sampleRegex = new Regex(@"//\[(?<sampleName>[^\s]+)[\r\n]+(?<sampleText>.+?)\r\n\s*//]", RegexOptions.Singleline);
+
+		private IDictionary<string, string> LoadSamples()
+		{
+			var samples = new Dictionary<string, string>();
+			foreach (var file in new DirectoryInfo(@".\SamplesForWiki").GetFiles("*.cs", SearchOption.AllDirectories))
+			{
+				string sampleContent = ReadContent(file);
+				foreach(Match match in sampleRegex.Matches(sampleContent))
+				{
+					string sampleName = match.Groups["sampleName"].Value;
+					string sampleText = match.Groups["sampleText"].Value;
+					Console.WriteLine(sampleText);
+					sampleText = MinimizeIdentation(sampleText);
+					Console.WriteLine(sampleText);
+					if(samples.ContainsKey(sampleName))
+						samples[sampleName] = samples[sampleName] + "\r\n" + sampleText;
+					else
+						samples.Add(sampleName, sampleText);
+				}
+			}
+			return samples;
+		}
+
+		private static string MinimizeIdentation(string sampleText)
+		{
+			string[] lines = sampleText.Split(new[]{"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+			var identation = lines.Min(l => MaxWhitespacePrefixLen(l));
+			Console.WriteLine("Identation " + identation);
+			return lines.Select(l => l.Substring(identation)).Aggregate("", (s, line) => s + "\r\n" + line);
+		}
+
+		private static int MaxWhitespacePrefixLen(string s)
+		{
+			for(int i=0; i<s.Length; i++)
+				if (s[i] != ' ' && s[i] != '\t') return i;
+			return s.Length;
+		}
+
+		private static void ProcessPage(FileInfo pageFile, IDictionary<string, string> samples)
+		{
+			string content = ReadContent(pageFile);
+			string result = new PageProcessor(content).Replace(samples);
+			SaveContent(pageFile, result);
+			
+		}
+
+		private static void SaveContent(FileInfo pageFile, string content)
+		{
+			using(var writer = new StreamWriter(pageFile.FullName))
+				writer.Write(content);
+		}
+
+		private static string ReadContent(FileInfo pageFile)
+		{
+			using(StreamReader reader = pageFile.OpenText())
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+
+	internal class PageProcessor
+	{
+		private readonly string input;
+		private readonly StringBuilder result;
+		private int index = 0;
+		private int lastReplacedTextEndIndex = 0;
+
+		public PageProcessor(string input)
+		{
+			this.input = input;
+			result = new StringBuilder(input.Length);
+		}
+
+		public string Replace(IDictionary<string, string> samplesDictionary)
+		{
+			while(MoveToEndOfFirst("<wiki:comment>["))
+			{
+				string sampleName = ReadNameUntil("</wiki:comment>");
+
+				if (!samplesDictionary.ContainsKey(sampleName)) throw new Exception("Несуществующий sample " + sampleName);
+				var text = "{{{" + samplesDictionary[sampleName] + "}}}";
+				ReplaceUntil("<wiki:comment />", text);
+			}
+			result.Append(input.Substring(lastReplacedTextEndIndex));
+			return result.ToString();
+		}
+
+		private void ReplaceUntil(string stopText, string replaceText)
+		{
+			result.Append(input.Substring(lastReplacedTextEndIndex, index - lastReplacedTextEndIndex));
+			result.Append(replaceText);
+			lastReplacedTextEndIndex = input.IndexOf(stopText, index);
+			index = lastReplacedTextEndIndex + stopText.Length;
+		}
+
+		private string ReadNameUntil(string stopText)
+		{
+			var startIndex = index;
+			index = input.IndexOf(stopText, index);
+			if (index < 0) throw new Exception("Closing text [" + stopText + "] required");
+			var name = input.Substring(startIndex, index - startIndex);
+			index += stopText.Length;
+			return name;
+		}
+
+		private bool MoveToEndOfFirst(string text)
+		{
+			index = input.IndexOf(text, index);
+			if(index < 0) index = input.Length;
+			index += text.Length;
+			return index < input.Length;
+		}
+	}
+}
