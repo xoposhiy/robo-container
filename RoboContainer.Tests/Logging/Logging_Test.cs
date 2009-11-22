73e70c37aa52d1d0429469bfea87d0c00ea0e792
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2009-11-22 15:28:06 +0000

    Тесты на логгирование.

---
+++ RoboContainer.Tests/Logging/Logging_Test.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using NUnit.Framework;
+using RoboContainer.Core;
+
+namespace RoboContainer.Tests.Logging
+{
+	[TestFixture]
+	public class Logging_Test
+	{
+		[Test]
+		public void can_disable_log()
+		{
+			var container = new Container(c => c.Logging.Disable());
+			container.Get<IExportFileScreen>();
+			Assert.AreEqual("", container.LastConstructionLog);
+		}
+
+		[Test]
+		public void can_use_userdefined_logger()
+		{
+			var container = new Container(c => c.Logging.UseLogger(new MockConstructionLogger()));
+			container.Get<IExportFileScreen>();
+			Assert.AreEqual("7", container.LastConstructionLog);
+		}
+
+		[Test]
+		public void LastConstructionLog()
+		{
+			var container = new Container();
+			container.Get<IExportFileScreen>();
+			Console.WriteLine(container.LastConstructionLog);
+			Assert.AreEqual(
+				@"Get IExportFileScreen
+	Get ExportFileScreen
+		Get IExporter[]
+			Get IExporter
+				Get BmpExporter
+				Constructed BmpExporter
+				Get JpgExporter
+				Constructed JpgExporter
+				Get PngExporter
+				Constructed PngExporter
+	Constructed ExportFileScreen
+",
+				container.LastConstructionLog);
+			container.Get<BmpExporter>();
+			Console.WriteLine(container.LastConstructionLog);
+			Assert.AreEqual(
+				@"Get BmpExporter
+	Reused BmpExporter
+",
+				container.LastConstructionLog);
+		}
+	}
+
+	public class MockConstructionLogger : IConstructionLogger
+	{
+		private int count;
+
+		public IDisposable StartConstruction(Type pluginType)
+		{
+			count++;
+			return new NullDisposable();
+		}
+
+		public override string ToString()
+		{
+			return count.ToString();
+		}
+
+		public void Constructed(Type pluggableType)
+		{
+		}
+
+		public void Initialized(Type pluggableType)
+		{
+		}
+
+		public void ConstructionFailed(Type pluggableType)
+		{
+		}
+
+		public void Reused(Type pluggableType)
+		{
+		}
+
+		public class NullDisposable : IDisposable
+		{
+			public void Dispose()
+			{
+			}
+		}
+	}
+
+	public interface IExportFileScreen
+	{
+	}
+
+	public class ExportFileScreen : IExportFileScreen
+	{
+		public ExportFileScreen(IExporter[] exporters)
+		{
+			exporters.ToString();
+		}
+	}
+
+	public interface IExporter
+	{
+	}
+
+	public class BmpExporter : IExporter
+	{
+	}
+
+	public class JpgExporter : IExporter
+	{
+	}
+
+	public class PngExporter : IExporter
+	{
+	}
+}
