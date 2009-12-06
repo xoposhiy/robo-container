1db4ef61a6cad3519628b70b2ca1b257e9b5bfe8
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2009-12-06 10:17:48 +0000

    Мегаоптимизация! Некоторые операции стали в разы быстрее. Типы из сборок больше не перебираются на каждый вызов Get.

---
+++ RoboContainer/Impl/TypesMap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoboContainer.Impl
+{
+	public class TypesMap
+	{
+		private readonly IDictionary<Type, IList<Type>> dic;
+
+		public TypesMap(IEnumerable<Type> types)
+		{
+			dic = new Dictionary<Type, IList<Type>>();
+			foreach(var type in types)
+				ProcessType(NormalizeGenericType(type));
+		}
+
+		private static Type NormalizeGenericType(Type type)
+		{
+			return type.IsGenericType ? type.GetGenericTypeDefinition() : type;
+		}
+
+		private void ProcessType(Type normalizedType)
+		{
+			if(!normalizedType.Constructable()) return;
+			foreach(var baseTypeOrInterface in normalizedType.GetBaseTypes().Concat(normalizedType.GetInterfaces()))
+				Inheritors(NormalizeGenericType(baseTypeOrInterface)).Add(normalizedType);
+		}
+
+		public IEnumerable<Type> GetInheritors(Type baseTypeOrInterface)
+		{
+			var t = dic.Keys.ToArray()[50];
+			return Inheritors(baseTypeOrInterface);
+		}
+
+		private IList<Type> Inheritors(Type baseTypeOrInterface)
+		{
+			return dic.GetOrCreate(baseTypeOrInterface, () => new List<Type>());
+		}
+	}
+}
