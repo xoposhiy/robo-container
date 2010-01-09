cdaa18d55e4350cfdc2ad7f03a7b4831ccdda068
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-01-09 17:23:10 +0000

    Пример для докуменации по коллекциям.

---
+++ trunk/RoboContainer.Tests/SamplesForWiki/CollectionSamples_Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using RoboContainer.Core;
+
+namespace RoboContainer.Tests.SamplesForWiki
+{
+	[TestFixture]
+	public class CollectionSamples_Test
+	{
+		//[Collections.Sample
+		public class Item {}
+		public class CollectionHolder
+		{
+			public Item[] Items { get; set; }
+
+			public CollectionHolder(Item[] items)
+			{
+				Items = items;
+			}
+		}
+
+		[Test]
+		public void can_inject_collections()
+		{
+			var container = new Container();
+			Assert.AreEqual(1, container.Get<Item[]>().Length);
+			Assert.AreEqual(1, container.Get<IEnumerable<Item>>().Count());
+			Assert.AreEqual(1, container.Get<ICollection<Item>>().Count());
+			Assert.AreEqual(1, container.Get<IList<Item>>().Count());
+			Assert.AreEqual(1, container.Get<CollectionHolder>().Items.Count());
+		}
+		//]
+	}
+}
