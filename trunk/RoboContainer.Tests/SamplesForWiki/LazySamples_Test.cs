4912f0b7b7061fccb170fc91a407a43032f24123
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-01-09 18:26:28 +0000

    Пример для докуменации по ленивым зависимостям.

---
+++ trunk/RoboContainer.Tests/SamplesForWiki/LazySamples_Test.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using RoboContainer.Core;
+
+namespace RoboContainer.Tests.SamplesForWiki
+{
+	[TestFixture]
+	public class LazySamples_Test
+	{
+		//[Lazy.Sample
+		public class Database
+		{
+			public static bool isCreated;
+
+			public Database()
+			{
+				isCreated = true;
+			}
+		}
+
+		public class SomeLogic
+		{
+			private readonly Lazy<Database> db;
+
+			public SomeLogic(Lazy<Database> db)
+			{
+				this.db = db;
+			}
+
+			public void DoSomething(bool useDb)
+			{
+				if(useDb)
+				{
+					// только тут контейнер создает реализацию Database
+					Database database = db.Get();
+					// ... do something with database
+					database.DontUse(); //hide
+				}
+				else
+				{
+					// do something without database
+					useDb.DontUse(); //hide
+				}
+			}
+		}
+
+		[Test]
+		public void lazy_dependency()
+		{
+			var container = new Container();
+			var someLogic = container.Get<SomeLogic>();
+			Assert.IsFalse(Database.isCreated);
+			someLogic.DoSomething(true); // только тут будет создана БД
+			Assert.IsTrue(Database.isCreated);
+		}
+		//]
+	}
+}
