8e290d660e76e07549b20b52c06f2c2bb00ba88e
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-01-14 18:38:43 +0000

    Сырая идея фабрик. Sample для документации по логгированию.

---
+++ trunk/RoboContainer.Tests/Factories/Factories_Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using RoboContainer.Core;
+using RoboContainer.Infection;
+
+namespace RoboContainer.Tests.Factories
+{
+	[TestFixture]
+	public class Factories_Test
+	{
+		[Test]
+		public void autoinject_factories()
+		{
+			var container = new Container();
+			var factory = container.Get<Factory<string, Foo>>();
+			Assert.IsNotNull(factory);
+			Assert.AreEqual("42", factory.Create("42").S);
+		}
+
+		public class Foo
+		{
+			public string S { get; set; }
+
+			public Foo(string s)
+			{
+				S = s;
+			}
+		}
+	}
+}
