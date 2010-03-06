bc8982270bccc3e2db993351017b9c07d7a95a35
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-03-06 07:41:27 +0000

    Сканирование сборок по умолчанию сменилось с ScanCallingAssembly на ScanLoadedCompanyAssemblies. Немного наведен порядок в коде и комментах.

---
+++ trunk/RoboContainer/Impl/AssembliesUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace RoboContainer.Impl
+{
+	public class AssembliesUtils
+	{
+		public static Assembly GetTheCallingAssembly()
+		{
+			Assembly thisAssembly = Assembly.GetExecutingAssembly();
+			StackFrame[] frames = new StackTrace(false).GetFrames() ?? new StackFrame[0];
+			return
+				frames
+					.Select(f => f.GetMethod().DeclaringType.Assembly)
+					.Where(a => a != thisAssembly)
+					.Where(a => a.FullName == null || 
+					            !a.FullName.StartsWith("system.", StringComparison.InvariantCultureIgnoreCase)
+					            && !a.FullName.StartsWith("mscorlib", StringComparison.InvariantCultureIgnoreCase)
+					)
+					.First();
+		}
+	}
+}
---
+++ trunk/RoboContainer/Impl/ExternalConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using RoboConfig;
+using RoboContainer.Core;
+
+namespace RoboContainer.Impl
+{
+	public class ExternalConfigurator : IExternalConfigurator
+	{
+		private readonly ContainerConfigurator configurator;
+
+		public ExternalConfigurator(ContainerConfigurator configurator)
+		{
+			this.configurator = configurator;
+		}
+
+		public void AppConfigSection(string sectionName)
+		{
+			XmlConfigurator.FromAppConfig(sectionName).ApplyConfigTo(configurator);
+		}
+
+		public void AppConfig()
+		{
+			AppConfigSection("robocontainer");
+		}
+
+		public void XmlFile(string filename)
+		{
+			XmlConfigurator.FromFile(filename).ApplyConfigTo(configurator);
+		}
+	}
+}
