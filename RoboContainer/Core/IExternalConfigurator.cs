421d92f6b40823e3335afd853ef9af1b0504de75
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-03-22 17:24:57 +0000

    Доработано конфигурирование через App.config. Написан тестовый проект для проверки.

--- RoboContainer/Core/IExternalConfigurator.cs
+++ RoboContainer/Core/IExternalConfigurator.cs
@@ -4,18 +4,55 @@
 	{
 		/// <summary>
 		/// Конфигурирует контейнер на основе секции <paramref name="sectionName"/> из app.config-файла.
-		/// Соответствующая секция должна быть... TODO секции в app.config
+		/// Секции должен быть сопоставлен обработчик "RoboContainer.RoboConfig.Settings, RoboContainer".
+		/// <para>
+		/// Пример файла app.config
+		/// <code>
+		/// <![CDATA[
+		/// <configuration>
+		///		<configSections>
+		/// 		<section name="robo" type="RoboContainer.RoboConfig.Settings, RoboContainer"/>
+		/// 	</configSections>
+		/// 	<robo>
+		/// 		<ForPlugin pluginType="TestAppConfig.IFoo, TestAppConfig">
+		///				<UsePluggable pluggableType="TestAppConfig.Foo2, TestAppConfig" />
+		/// 		</ForPlugin>
+		/// 	</robo>
+		/// </configuration>
+		/// ]]>
+		/// </code>
+		/// </para>
+		/// <seealso cref="AppConfig"/>
+		/// <seealso cref="XmlFile"/>
 		/// </summary>
 		void AppConfigSection(string sectionName);
 
 		/// <summary>
 		/// Конфигурирует контейнер на основе секции 'robocontainer' из app.config-файла.
-		/// Соответствующая секция должна быть... TODO секции в app.config
+		/// Аналог вызова <c>AppConfigSection("roboconfig")</c>
 		/// </summary>
+		/// <seealso cref="AppConfigSection"/>
+		/// <seealso cref="XmlFile"/>
 		void AppConfig();
 	
 		/// <summary>
 		/// Конфигурирует контейнер на основе секции Xml-файла <paramref name="filename"/>.
+		/// <para>Пример xml-файла
+		/// <code>
+		/// <![CDATA[
+		/// 	<robo>
+		/// 		<ForPlugin pluginType="TestAppConfig.IFoo, TestAppConfig">
+		///				<UsePluggable pluggableType="TestAppConfig.Foo2, TestAppConfig" />
+		/// 		</ForPlugin>
+		/// 	</robo>
+		/// ]]>
+		/// </code>
+		/// </para>
+		/// <para>
+		/// Синтаксис xml полностью повторяет синтаксис конфигурирования кодом.
+		/// </para>
+		/// <seealso cref="AppConfig"/>
+		/// <seealso cref="AppConfigSection"/>
 		/// </summary>
 		void XmlFile(string filename);
 	}
