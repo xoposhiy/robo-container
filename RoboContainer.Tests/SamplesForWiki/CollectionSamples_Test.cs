df29a5b8a7eaf72e73878256bf0cadf8fa9ed151
xoposhiy@mail.ru <xoposhiy@mail.ru@907af9e4-cc46-11de-9a23-210ccc102f2f>
2010-03-27 05:06:57 +0000

    Доработка документации

--- RoboContainer.Tests/SamplesForWiki/CollectionSamples_Test.cs
+++ RoboContainer.Tests/SamplesForWiki/CollectionSamples_Test.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using RoboContainer.Core;
 
@@ -9,27 +8,50 @@ namespace RoboContainer.Tests.SamplesForWiki
 	public class CollectionSamples_Test
 	{
 		//[Collections.Sample
-		public class Item {}
+		public class Item
+		{
+		}
+
 		public class CollectionHolder
 		{
-			public Item[] Items { get; set; }
+			public Item[] Items_AsArray { get; set; }
+			public IEnumerable<Item> Items_AsEnumerable { get; set; }
+			public ICollection<Item> Items_AsCollection { get; set; }
+			public IList<Item> Items_AsList { get; set; }
 
-			public CollectionHolder(Item[] items)
+			public CollectionHolder(Item[] itemsAsArray, IEnumerable<Item> itemsAsEnumerable, ICollection<Item> itemsAsCollection, IList<Item> itemsAsList)
 			{
-				Items = items;
+				Items_AsArray = itemsAsArray;
+				Items_AsEnumerable = itemsAsEnumerable;
+				Items_AsCollection = itemsAsCollection;
+				Items_AsList = itemsAsList;
 			}
 		}
 
 		[Test]
-		public void can_inject_collections()
+		public void get_collections()
 		{
 			var container = new Container();
-			Assert.AreEqual(1, container.Get<Item[]>().Length);
-			Assert.AreEqual(1, container.Get<IEnumerable<Item>>().Count());
-			Assert.AreEqual(1, container.Get<ICollection<Item>>().Count());
-			Assert.AreEqual(1, container.Get<IList<Item>>().Count());
-			Assert.AreEqual(1, container.Get<CollectionHolder>().Items.Count());
+			var expectedItems = new[] {container.Get<Item>()};
+			CollectionAssert.AreEqual(expectedItems, container.Get<Item[]>());
+			CollectionAssert.AreEqual(expectedItems, container.Get<IList<Item>>());
+			CollectionAssert.AreEqual(expectedItems, container.Get<ICollection<Item>>());
+			CollectionAssert.AreEqual(expectedItems, container.Get<IEnumerable<Item>>());
+			CollectionAssert.AreEqual(expectedItems, container.GetAll<Item>()); //эквивалент предыдущей строки
 		}
+
+		[Test]
+		public void inject_collections()
+		{
+			var container = new Container();
+			var collectionHolder = container.Get<CollectionHolder>();
+			var expectedItems = container.GetAll<Item>();
+			CollectionAssert.AreEqual(expectedItems, collectionHolder.Items_AsArray);
+			CollectionAssert.AreEqual(expectedItems, collectionHolder.Items_AsEnumerable);
+			CollectionAssert.AreEqual(expectedItems, collectionHolder.Items_AsCollection);
+			CollectionAssert.AreEqual(expectedItems, collectionHolder.Items_AsList);
+		}
+
 		//]
 	}
 }
